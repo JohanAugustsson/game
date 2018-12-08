e7c50afbefbe28dbe4424422470cf26d5ddc41eb
Peter Augustsson <peter.augustsson@altrusoft.com>
2018-12-08 12:50:58 +0100

    Lagt till storybook addons samt enkel komponent för player add substract

---
+++ storybook/stories/playerAddScore.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {storiesOf} from "@storybook/react";
+import {action} from '@storybook/addon-actions';
+import PlayerAddSub from "../../src/components/players/PlayerAddSub";
+
+export const player = {
+    id: '1',
+    name: 'Bill',
+    value: '1',
+};
+
+export const actions = {
+    onAdd: action('onAdd'),
+    onSubstract: action('onSubstract'),
+};
+
+
+storiesOf('Player', module).add('default', () => <PlayerAddSub task={player} {...actions} />);
