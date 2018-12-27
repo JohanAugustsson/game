3dc014bd0682e67269978939131612c1f2f19b33
Peter Augustsson <peter.augustsson@altrusoft.com>
2018-12-27 14:26:33 +0100

    playeraddsub ny layout materialui

--- src/components/players/PlayerAddSub.js
+++ src/components/players/PlayerAddSub.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './playerAddSub.css'
+import Grid from "@material-ui/core/Grid";
+import PersonIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import PersonIcon from "@material-ui/icons/Person";
 
-function PlayerAddSub({player: {firstName, uid, value}, onAdd, onSubstract}) {
+function PlayerAddSub({player, onAdd, onSubstract}) {
     return (
-        <div className="game-form">
-            <div className="margin"> {firstName}</div>
-            <div className="margin">
-                <button onClick={() => onSubstract(uid)}> -</button>
-            </div>
-            <div className="margin">
-                {value}</div>
-            <div className="margin">
-                <button onClick={() => onAdd(uid)}> +</button>
-            </div>
-        </div>
+        <Grid item xs container direction={"row"}
+              alignItems={"center"} justify={"center"}>
+            <Grid item xs={1} sm={2} className={"gridcenter"}>
+                <PersonIcon/>
+            </Grid>
+            <Grid item xs={8} sm={6}>
+                <span>{player.firstName}</span>
+            </Grid>
+            <Grid item xs={1} sm={1} className={"gridcenter"}>
+                <IconButton aria-label="Delete"
+                            onClick={() => onSubstract(player)}>
+                    <RemoveIcon/>
+                </IconButton>
+            </Grid>
+            <Grid item xs={1} sm={2} className={"gridcenter"}>
+                <Typography variant={"subtitle1"}
+                            align={"center"}
+                            className={"gridcenter"}> {player.value}</Typography>
+            </Grid>
+            <Grid item xs={1} sm={1} className={"gridcenter"}>
+                <IconButton aria-label="Add" onClick={() => onAdd(player)}>
+                    <AddIcon/>
+                </IconButton>
+            </Grid>
+        </Grid>
     )
 }
 
