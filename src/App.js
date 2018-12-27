6825389358c323385b5d807d5fe75fc88fa93051
Peter Augustsson <peter.augustsson@altrusoft.com>
2018-12-27 22:28:04 +0100

    meny implementerad

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
-import Router from './Router';
-import { Switch, Route } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import ResponsiveDrawer from "./components/responsiveDrawer/responsiveDrawer";
 import SnackBar from './components/snackbar/Snackbar'
-const Navigation = (props) => (
-  <nav>
-    <ul>
-      <li><NavLink to='/'>Skapa användare</NavLink></li>
-      <li><NavLink to='/create-group'>Skapa Grupp</NavLink></li>
-      <li><NavLink to='/create-game'>Skapa game</NavLink></li>
-      <li><NavLink to='/game'>Game</NavLink></li>
-    </ul>
-  </nav>
-)
+
+const Navigation = () => (
+    <ResponsiveDrawer/>
+);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="page-container">
-        <Navigation />
-        <Router />
-        <SnackBar />
-      </div>
-    )
-  };
+    render() {
+        return (
+            <div className="page-container">
+                <Navigation/>
+                <SnackBar/>
+            </div>
+        )
+    };
 }
 
 export default App;
